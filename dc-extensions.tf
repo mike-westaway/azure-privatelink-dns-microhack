a54abad30e351044ba7b01b96107200d63b6d1ee
Mike Westaway <mike.x.westaway@gsk.com>
2021-04-07 12:47:29 +0100

    defined locals

--- dc-extensions.tf
+++ dc-extensions.tf
@@ -5,6 +5,25 @@
 ## Kudos to Chris Walden for this article https://cloudblogs.microsoft.com/industry-blog/en-gb/technetuk/2016/06/08/setting-up-active-directory-via-powershell/
 ##########################################################
 
+
+##########################################################
+# 
+# Define locals
+#
+##########################################################
+
+locals {
+  feature_flags = {
+      provision_domaincontroller = false
+  }
+}
+
+##########################################################
+# 
+# Define resources
+#
+##########################################################
+
 resource "azurerm_virtual_machine_extension" "install-ds-onprem-dc" {
 
   # Conditionally based on feature flag
@@ -16,12 +35,15 @@ resource "azurerm_virtual_machine_extension" "install-ds-onprem-dc" {
   type                 = "CustomScriptExtension"
   type_handler_version = "1.9"
 
-  settings = <<SETTINGS
+  provisioner "local-exec" {
+    command = <<EOT
         install-windowsfeature AD-Domain-Services
         Import-Module ADDSDeployment
         Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "WinThreshold" -DomainName "onprem.org" -DomainNetbiosName "ONPREM" -ForestMode "WinThreshold" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$false -SysvolPath "C:\Windows\SYSVOL" -Force:$true
         exit 0"
-SETTINGS
-  interpreter = ["PowerShell", "-ExecutionPolicy Unrestricted -Command"]
+    EOT
+
+    interpreter = ["PowerShell", "-ExecutionPolicy Unrestricted -Command"]
+  }
 }
 
